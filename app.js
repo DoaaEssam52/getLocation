26cbd2d152c7d10fb7659a9bcf1d89dad43cfe93
DoaaEssam52 <32943933+DoaaEssam52@users.noreply.github.com>
2020-01-26 19:10:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function getLocationData()
         navigator.geolocation.getCurrentPosition(position=>
         {
                  //variables
-                var txt1,txt2,txt3,txt4;
+                var txt1,txt2,txt3,txt4,txt5;
                 //جلب دائرة العرض
                 txt1 = position.coords.latitude;
                  // جلب خط الطول
@@ -19,5 +19,10 @@ function getLocationData()
                 document.getElementById("txt2").value=txt2;
                 document.getElementById("txt3").value=txt3;
                 document.getElementById("txt4").value=txt4; 
+                
+                $.get("https://api.ipdata.co?api-key=test", function(response) {
+                txt5=response.city + ", " + response.region;
+                console.log(txt5)
+                }, "jsonp");
         });
 }
